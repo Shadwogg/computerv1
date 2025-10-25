9227c04d03069facedc8b23be26724a2bb1687c7
Guillaume Giboury <ggiboury@k0r3p11.42mulhouse.fr>
2025-10-25 17:16:58 +0200

    .

--- Term.cpp
+++ Term.cpp
@@ -6,13 +6,15 @@
 /*   By: ggiboury <ggiboury@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 22:44:14 by ggiboury          #+#    #+#             */
-/*   Updated: 2025/10/20 22:46:19 by ggiboury         ###   ########.fr       */
+/*   Updated: 2025/10/25 16:16:15 by ggiboury         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Term.hpp"
 
-Term::Term(){
+Term::Term(std::string inp){
     std::cout << this->opd << this->op << std::endl;
+    std::cout << inp << std::endl;    
 }
+
 Term::~Term(){}
--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -6,7 +6,7 @@
 /*   By: ggiboury <ggiboury@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 23:55:04 by ggiboury          #+#    #+#             */
-/*   Updated: 2025/10/25 15:16:21 by ggiboury         ###   ########.fr       */
+/*   Updated: 2025/10/25 17:16:30 by ggiboury         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,42 @@ std::string	ft_clean_input(std::string inp){
 			res.append(1, inp[i]);
 		i++;
 	}
-	std::cout << "Cleaned " << res << std::endl;
+	// std::cout << "Cleaned " << res << std::endl;
 	return (res);
+}
+
+void	verify_uniq_litt(std::string inp){
+	size_t i = 0;
+	
+	size_t pos = inp.find_first_of("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+	std::cout << (pos == std::string::npos);
+	
+	if (pos == std::string::npos){
+		std::cout << "No litteral found (where is the x ?)";
+		throw (std::exception());
+	}
+	
+	while (inp[i]){
+		// std::cout << "Verif :" << pos << inp[i] << std::endl;
+		if (isalpha(inp[i]) && inp[pos] != inp[i])
+			throw(std::exception());
+		i++;
+	}
+}
+
+Operand	*extract_number(std::string const &inp, size_t start){
+	// Int
+	(void) inp;
+	(void) start;
+	//Real
+	return (NULL);
 }
 
 std::list<Term> tokenify(std::string inp){
 	size_t i = 0;
 	inp = ft_clean_input(inp);
+	
+	verify_uniq_litt(inp); // Verify that we have at least 1 Unknown
 	std::list<Term> tokens;
 	std::cout << inp << std::endl;
     
@@ -36,8 +65,13 @@ std::list<Term> tokenify(std::string inp){
 	while (inp[i]){
 		//case Number
 		if (isalnum(inp[i])){
-			std::cout << "We are number one" << std::endl;
+			std::cout << "We one" << std::endl;
+			Operand	*opd = extract_number(inp, i);
+			(void) opd;
 			i++;
+			while (isalpha(inp[i]))
+				i++;
+			// Case operator
 		} else if (is_operator(inp[i])){
 			std::cout << "Operatordation" << std::endl;
             op = new Operator(inp[i]);
